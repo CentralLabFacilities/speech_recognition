e28da40bf3375018ec0acae608d9809a03a04b88
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2024-04-25 16:54:58 +0200

    test model script

---
+++ ros_rasa/scripts/test_nlu_model.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+
+
+import argparse
+
+import sys
+import asyncio
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test Rasa NLU Data")
+    parser.add_argument(
+        "-n",
+        "--nlu",
+        help="NLU file",
+        type=argparse.FileType("r"),
+    )
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Config file",
+        type=argparse.FileType("r"),
+    )
+
+    args = parser.parse_args()
+
+    if not args.nlu or not args.config:
+        parser.print_usage()
+        sys.exit(1)
+
+
+    from rasa.model_training import train_nlu
+    from rasa.engine.constants import PLACEHOLDER_MESSAGE
+    from rasa.core.channels import UserMessage
+    from rasa.core.processor import MessageProcessor
+
+    mod = train_nlu(
+        nlu_data=args.nlu.name,
+        config=args.config.name,
+        output="/tmp/rasa",
+    )
+
+    print("Model finished training")
+    _, model_metadata, graph_runner = MessageProcessor._load_model(mod)
+
+    print("""
+#######################
+Model Trined and Loaded
+#######################
+
+          """)
+
+    while True:
+        var = input("Please enter something: ")
+        message = UserMessage(var)
+
+        results = graph_runner.run(
+            inputs={PLACEHOLDER_MESSAGE: [message]},
+            targets=[model_metadata.nlu_target]
+        )
+        for output in results[model_metadata.nlu_target]:
+            kv = output.as_dict()
+            print(f"full output: {kv}")
+            print(
+                f"\nInput: '{kv['text']}' intent: {kv['intent']['name']} conf: {kv['intent']['confidence']}"
+            )
+            for entity in kv["entities"]:
+                print(f"Entity: '{entity['value']}' type '{entity['entity']}'")
+            print()
