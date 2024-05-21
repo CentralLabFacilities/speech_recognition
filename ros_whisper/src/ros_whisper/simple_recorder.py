adc4d34ea8b4b43000344d69c4da76c30d93fdb9
lruegeme <lruegeme@techfak.uni-bielefeld.de>
2024-05-21 14:53:30 +0200

    wip

---
+++ ros_whisper/src/ros_whisper/simple_recorder.py
@@ -0,0 +1,31 @@
+import rospy
+import numpy as np
+from audio_common_msgs.msg import AudioData, AudioInfo
+
+
+class SimpleRecorder:
+
+    def __init__(self):
+        self.samplerate = 16000
+
+        rospy.loginfo(logger_name="SimpleRecorder", msg=f"waiting for audio_info...")
+        audio_info: AudioInfo = rospy.wait_for_message(
+            "audio_info", AudioInfo, timeout=5
+        )  # type: ignore
+
+        if not audio_info.sample_rate == 16000:
+            msg = f"Sample rate needs to be 16000"
+            rospy.logerr(logger_name="SimpleRecorder", msg=msg)
+            raise Exception(msg)
+
+        if not audio_info.channels == 1:
+            msg = f"Cant use non mono audio"
+            rospy.logerr(logger_name="SimpleRecorder", msg=msg)
+            raise Exception(msg)
+
+        if audio_info.sample_format.lower() == "f32le":
+            self.depth_type = np.float32
+        else:
+            msg = f"unhandled audio format"
+            rospy.logerr(logger_name="SimpleRecorder", msg=msg)
+            raise Exception(msg)
---
+++ silero_vad/src/silero_vad/cached_silero.py
@@ -0,0 +1,81 @@
+import os.path
+import rospy
+
+from typing import Any
+
+
+class SileroVAD:
+    samplerate = 16000
+
+    def __init__(
+        self,
+        threshold: float = 0.5,
+        min_silence_duration_ms: int = 500,
+        speech_pad_ms: int = 10,
+    ):
+        try:
+            # try loading from cache
+            import sys
+
+            cache = os.path.abspath(
+                os.path.expanduser("~/.cache/torch/hub/snakers4_silero-vad_master")
+            )
+            sys.path.append(cache)
+            from utils_vad import (
+                get_speech_timestamps,
+                save_audio,
+                read_audio,
+                VADIterator,
+                collect_chunks,
+                OnnxWrapper,
+            )
+
+            model_dir = os.path.join(cache, "files")
+            model = OnnxWrapper(os.path.join(model_dir, "silero_vad.onnx"), False)
+            utils = (
+                get_speech_timestamps,
+                save_audio,
+                read_audio,
+                VADIterator,
+                collect_chunks,
+            )
+            rospy.loginfo(logger_name="SileroVAD", msg=f"loaded model from cache")
+        except Exception as e:
+            rospy.loginfo(logger_name="SileroVAD", msg=f"loading from cache failed...")
+            model, utils = torch.hub.load(
+                repo_or_dir="snakers4/silero-vad",
+                model="silero_vad",
+                force_reload=False,
+                onnx=True,
+            )
+
+        (_, _, _, self.VADIterator, _) = utils
+        self.silero_model = model
+        self.setup_vad()
+        self.vad_iterator = VADIterator(
+            model,
+            threshold,
+            self.samplerate,
+            min_silence_duration_ms,
+            speech_pad_ms,
+        )
+
+    def setup_vad(
+        self,
+        threshold: float = 0.5,
+        min_silence_duration_ms: int = 300,
+        speech_pad_ms: int = 500,
+    ):
+        self.vad_iterator = self.VADIterator(
+            self.silero_model,
+            threshold,
+            self.samplerate,
+            min_silence_duration_ms,
+            speech_pad_ms,
+        )
+
+    def reset_states(self):
+        self.vad_iterator.reset_states()
+
+    def __call__(self, x):
+        return self.vad_iterator(x)
