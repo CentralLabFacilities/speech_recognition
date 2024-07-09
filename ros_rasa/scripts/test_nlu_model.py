cf934164fa6b5c87d58480e82cd125f80f6d1602
lruegeme <lruegeme@techfak.uni-bielefeld.de>
2024-07-09 17:37:52 +0200

    rasa: test_nlu_model has nicer cli
    - now supports --model to load pretrained
    - --verbose to print dict
    - now supports groups
    - print intent rankings

--- ros_rasa/scripts/test_nlu_model.py
+++ ros_rasa/scripts/test_nlu_model.py
@@ -6,6 +6,11 @@ import argparse
 import sys
 import asyncio
 
+try:
+    import readline
+except:
+    pass  # readline not available
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test Rasa NLU Data")
     parser.add_argument(
@@ -22,28 +27,47 @@ if __name__ == "__main__":
         type=argparse.FileType("r"),
     )
 
-    args = parser.parse_args()
-
-    if not args.nlu or not args.config:
-        parser.print_usage()
-        sys.exit(1)
-
-    from rasa.model_training import train_nlu
-    from rasa.engine.constants import PLACEHOLDER_MESSAGE
-    from rasa.core.channels import UserMessage
-    from rasa.core.processor import MessageProcessor
-
-    mod = train_nlu(
-        nlu_data=args.nlu.name,
-        config=args.config.name,
-        output="/tmp/rasa",
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="Model file",
+        type=argparse.FileType("r"),
     )
 
-    print("Model finished training")
-    _, model_metadata, graph_runner = MessageProcessor._load_model(mod)
+    parser.add_argument(
+        "-v", "--verbose", help="Print the returned dict", action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    if (args.model and args.config) or (args.model and args.config):
+        print("cant use model with config/nlu")
+        sys.exit(1)
+
+    from rasa.core.channels import UserMessage
+    from rasa.core.processor import MessageProcessor
+    from rasa.engine.constants import PLACEHOLDER_MESSAGE
+
+    if args.model:
+        _, model_metadata, graph_runner = MessageProcessor._load_model(args.model.name)
+    else:
+        if not args.nlu or not args.config:
+            parser.print_usage()
+            sys.exit(1)
+
+        from rasa.model_training import train_nlu
+
+        mod = train_nlu(
+            nlu_data=args.nlu.name,
+            config=args.config.name,
+            output="/tmp/rasa",
+        )
+
+        print("Model finished training")
+        _, model_metadata, graph_runner = MessageProcessor._load_model(mod)
 
     print(
-"""
+        """
 ########################
 Model Trained and Loaded
 ########################
@@ -59,17 +83,26 @@ Model Trained and Loaded
         )
         for output in results[model_metadata.nlu_target]:
             kv = output.as_dict()
-            print(f"full output: {kv}")
+
+            if args.verbose:
+                print(f"\nfull output:\n  {kv}")
+
+            print(f"\nintent ranking:")
+            for intent in kv["intent_ranking"]:
+                print(f"  - '{intent['name']}', conf:'{intent['confidence']:1.2f}'")
+
             print(
                 f"\nInput: '{kv['text']}' intent: {kv['intent']['name']} conf: {kv['intent']['confidence']}"
             )
             for entity in kv["entities"]:
+                text = f"  Entity:'{entity['value']}', type:'{entity['entity']}':{entity['confidence_entity']:1.2f}"
                 if "role" in entity:
-                    print(
-                        f"  Entity:'{entity['value']}', type:'{entity['entity']}':{entity['confidence_entity']:1.2f}, role:'{entity['role']}':{entity['confidence_role']:1.2f}"
+                    text += (
+                        f", role:'{entity['role']}':{entity['confidence_role']:1.2f}"
                     )
-                else:
-                    print(
-                        f"  Entity:'{entity['value']}', type:'{entity['entity']}':{entity['confidence_entity']:1.2f}"
+                if "group" in entity:
+                    text += (
+                        f", group:'{entity['group']}':{entity['confidence_role']:1.2f}"
                     )
+                print(text)
             print()
