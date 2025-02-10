d8f7360ab91f0d2b61f597b5d7795df4ce25d694
lruegeme <lruegeme@techfak.uni-bielefeld.de>
2025-02-10 13:38:34 +0100

    fixup! add piper and nllb pkgs

--- ros_piper_tts/setup.py
+++ ros_piper_tts/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=["piper_tts"], package_dir={"": "src"})
+d = generate_distutils_setup(packages=["ros_piper_tts"], package_dir={"": "src"})
 
 setup(**d)
